0438bac4314be860fd1f8a9d8bbb8fecefaed16b
Vlad-Yarko <vladyarik88888@gmail.com>
2025-08-22 20:50:53 +0300

    ModelAdmin

--- mydjango/myorm/admin.py
+++ mydjango/myorm/admin.py
@@ -3,9 +3,33 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Author)
-admin.site.register(models.Post)
-admin.site.register(models.User)
-admin.site.register(models.Profile)
-admin.site.register(models.Student)
-admin.site.register(models.Course)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", )
+    
+    
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    
+    
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("level", )
+    
+    
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    
+    
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", )
+
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Profile, ProfileAdmin)
+admin.site.register(models.Student, StudentAdmin)
+admin.site.register(models.Course, CourseAdmin)
